945c364aec4ec7d5b45bb9d13ba16ce918004156
Bibi Khadiza <limakhadiza@yahoo.com>
2016-07-25 14:41:57 -0400

    done

---
+++ lib/song.rb
@@ -0,0 +1,20 @@
+class Song
+  attr_accessor :name, :artist
+
+
+  def initialize(name)
+    @name = name
+
+  end
+
+  def artist_name
+    return self.artist.name unless artist.nil?
+
+  end
+
+
+
+
+
+
+end
