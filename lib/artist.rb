5415879899d0c63784c57cb59c4ab866b166408d
Ekaterina Petushova <epetushova@gmail.com>
2016-08-01 15:54:21 -0400

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,31 @@
+require 'pry'
+
+
+class Artist
+	@@song_count = 0
+
+	attr_accessor :name, :songs
+
+	def initialize(name)
+		@name = name
+		@songs = []
+	end
+
+	def add_song(song)
+    	@songs << song
+    	@@song_count +=1
+    	# binding.pry
+ 		song.artist = self
+  	end
+
+  	def add_song_by_name(name)
+	    song = Song.new(name)
+	    @songs << song
+	    @@song_count +=1
+	    song.artist = self
+    end
+
+    def self.song_count
+    	@@song_count
+    end
+end
