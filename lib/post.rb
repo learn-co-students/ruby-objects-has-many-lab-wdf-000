d529a24b51dca84b89803ca8cd111b906fd01595
dimacodes <dimitriserov@gmail.com>
2016-08-25 15:54:26 -0400

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,18 @@
+class Post
+
+  attr_accessor :title, :author
+
+  def initialize(title)
+    @title = title
+    @author = author
+  end
+
+  def author_name
+    if @author
+      self.author.name
+    end
+  end
+
+
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,15 @@
+class Song
+
+  attr_accessor :name, :artist
+
+  def initialize(name)
+    @name = name
+    @artist = artist
+  end
+
+  def artist_name
+    if @artist
+      self.artist.name
+    end
+  end
+end
